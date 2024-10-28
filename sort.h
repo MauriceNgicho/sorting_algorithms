ac1e88e72a2ae0f625fff4134b6de7b9ef8e56c2
MauriceNgicho <mrcngicho@gmail.com>
2024-10-28 06:52:22 -0700

    Insertion sort

--- sort.h
+++ sort.h
@@ -3,8 +3,23 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif /*SORT_H*/
